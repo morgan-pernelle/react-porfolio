d339907c8b3f9472dfa11d030caf51faeeddcfc0
Morgan Pernelle <pernelle.morgan@gmail.com>
2023-01-20 13:28:39 +0100

    testimonials

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import DotGroup from "./scenes/DotGroup";
 import Landing from "./scenes/Landing";
 import MySkills from "./scenes/MySkills";
 import Projects from "./scenes/Projects";
+import Testimonials from "./scenes/Testimonials";
 import { useState, useEffect } from "react";
 import LineGradient from "./components/LineGradient";
 import useMediaQuery from "./hooks/useMediaQuery";
@@ -43,6 +44,10 @@ function App() {
     <LineGradient />
     <div className="w-5/6 mx-auto">
       <Projects />
+    </div>
+    <LineGradient />
+    <div className="w-5/6 mx-auto md:h-full">
+      <Testimonials />
     </div>
   </div>;
 }
